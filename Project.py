4e6cbf0f4a7912469c066f239b5ac01b4c61621c
nikita-kuprin <nik.kuprin.02@mail.ru>
2020-01-12 15:42:06 +0300

    the basis of the game

--- Project.py
+++ Project.py
@@ -1,1 +1,143 @@
-print("test")
+import pygame
+import sys
+import random
+import os
+
+pygame.init()
+
+FPS = 60
+
+WIDTH = 500
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+enemy = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    image = image.convert_alpha()
+
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(player_img, (60, 30))
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey(pygame.Color('White'))
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def shooting(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(meteor_img, (60, 55))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+background = load_image('background.png')
+background_rect = background.get_rect()
+player_img = load_image('rocket.png')
+meteor_img = load_image('meteor.png')
+bullet_img = load_image('bullet.png')
+screen.blit(background, background_rect)
+player = Player()
+all_sprites.add(player)
+for i in range(8):
+    m = Enemy()
+    all_sprites.add(m)
+    enemy.add(m)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shooting()
+
+    all_sprites.draw(screen)
+    all_sprites.update()
+
+    hits = pygame.sprite.spritecollide(player, enemy, False)
+    if hits:
+        running = False
+    screen.fill(pygame.Color("Black"))
+    screen.blit(background, background_rect)
+    all_sprites.draw(screen)
+
+    hits = pygame.sprite.groupcollide(enemy, bullets, True, True)
+    for hit in hits:
+        m = Enemy()
+        all_sprites.add(m)
+        enemy.add(m)
+
+    pygame.display.flip()
+
+    clock.tick(FPS)
+
+terminate()
